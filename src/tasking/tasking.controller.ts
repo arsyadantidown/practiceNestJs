ee97c0546699e288ec3119819b1e026267d27fa0
arsyadantidown <superciouskid@gmail.com>
2026-02-08 18:20:14 +0700

    Update tasking.controller.ts

--- src/tasking/tasking.controller.ts
+++ src/tasking/tasking.controller.ts
@@ -1,14 +1,21 @@
-import { Controller, Post, Body, Get, Param, ParseIntPipe, Delete} from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Patch, ParseIntPipe, Delete} from '@nestjs/common';
 import { TaskingService } from './tasking.service';
 import { CreateTaskDto } from './DTO/createTask.dto';
+import {UpdateTaskDto} from './DTO/updateTask.dto'
 
-@Controller('task')
-export class TaskingController {
-  constructor(private readonly taskingService: TaskingService) {}
+
+  @Controller('task')
+  export class TaskingController {
+    constructor(private readonly taskingService: TaskingService) {}
 
   @Post()
   createTask(@Body()createTaskDto:CreateTaskDto){
     return this.taskingService.createTask(createTaskDto)
+  }
+
+  @Patch(':id')
+  updateTask(@Param('id',ParseIntPipe)id:number,@Body()updateTaskDto:UpdateTaskDto){
+    return this.taskingService.updateTask(id,updateTaskDto)
   }
 
   @Get()
